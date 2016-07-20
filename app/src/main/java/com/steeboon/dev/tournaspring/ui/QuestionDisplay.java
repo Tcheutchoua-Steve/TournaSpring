05279b5832ecd53736a8e5c9140a7a498d2c7ba9
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2016-07-20 14:35:32 +0100

    first question displayed on the screen , hurray

--- app/src/main/java/com/steeboon/dev/tournaspring/ui/QuestionDisplay.java
+++ app/src/main/java/com/steeboon/dev/tournaspring/ui/QuestionDisplay.java
@@ -5,20 +5,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.steeboon.dev.tournaspring.R;
+import com.steeboon.dev.tournaspring.util.QuestionList;
 
 public class QuestionDisplay extends AppCompatActivity {
 
     // Creating questions view variables
-    private RadioGroup radioQuestion ;
-    private RadioButton radioAnswer1;
-    private RadioButton radioAnswer2;
-    private RadioButton radioAnswer3;
-    private RadioButton radioAnswer4;
+    private RadioGroup radio_group_question ;
+    private TextView    radio_question;
+    private RadioButton radio_answer1;
+    private RadioButton radio_answer2;
+    private RadioButton radio_answer3;
+    private RadioButton radio_answer4;
+
+    private QuestionList  allquestions ;
+    private int number_of_questions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +34,31 @@ public class QuestionDisplay extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        radioQuestion = (RadioGroup)findViewById(R.id.radioQuestion);
-        radioAnswer1 = (RadioButton)findViewById(R.id.radioAnswer1);
-        radioAnswer2 = (RadioButton)findViewById(R.id.radioAnswer2);
-        radioAnswer3 = (RadioButton)findViewById(R.id.radioAnswer3);
-        radioAnswer4 = (RadioButton)findViewById(R.id.radioAnswer4);
+        radio_group_question = (RadioGroup)findViewById(R.id.radioGroupQuestion);
+        radio_question = (TextView)findViewById(R.id.radioQuestion);
+        radio_answer1 = (RadioButton)findViewById(R.id.radioAnswer1);
+        radio_answer2 = (RadioButton)findViewById(R.id.radioAnswer2);
+        radio_answer3 = (RadioButton)findViewById(R.id.radioAnswer3);
+        radio_answer4 = (RadioButton)findViewById(R.id.radioAnswer4);
 
 
+        Bundle  b = getIntent().getExtras(); // Get the intent's extras
+
+        allquestions = b.getParcelable("questionList");
+
+        Log.i("num_questions",String.valueOf(allquestions.size()));
+        Log.i("allquestions",allquestions.toString());
+
+
+        allquestions = b.getParcelable("questionList");
+
+        Log.i("num_questions",String.valueOf(allquestions.size()));
+        Log.i("allquestions",allquestions.toString());
+        radio_question.setText(allquestions.get(1).getQuestion_asked());
+        radio_answer1.setText(allquestions.get(1).getAnswer1());
+        radio_answer2.setText(allquestions.get(1).getAnswer2());
+        radio_answer3.setText(allquestions.get(1).getAnswer3());
         
-
-
-
-
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
